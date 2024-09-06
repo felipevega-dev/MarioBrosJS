0704fe49ea42ee53cb4e1241eb8929f8143bb05d
Felipe <felipevega.dev@gmail.com>
2024-09-06 17:58:25 -0400

    Animaciones arregladas, primer enemigo, segmentación de codigo.

--- game.js
+++ game.js
@@ -1,128 +1,132 @@
-/* global Phaser */
-
-import { createAnimations } from "./animations.js";
+import { createAnimations } from "./animations.js"
+import { checkControls } from "./controls.js"
 
 const config = {
-    type: Phaser.AUTO,
-    width: 256,
-    height: 244,
-    backgroundColor: '#049cd8',
-    parent: 'game',
-    physics: {
-        default: 'arcade',
-        arcade : {
-            gravity: { y: 300},
-            debug: false
-
-        }
-    },
-    scene: {
-        preload, // se ejecuta antes de crear el juego
-        create, // se ejecuta al crear el juego
-        update // se ejecuta en cada frame
+  type: Phaser.AUTO, // webgl, canvas
+  width: 256,
+  height: 244,
+  backgroundColor: '#049cd8',
+  parent: 'game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
     }
+  },
+  scene: {
+    preload, // se ejecuta para precargar recursos
+    create, // se ejecuta cuando el juego comienza
+    update // se ejecuta en cada frame
+  }
 }
 
-new Phaser.Game(config);
-// this -> game -> scene -> object
+new Phaser.Game(config)
+// this -> game -> el juego que estamos construyendo
 
-function preload() {
-    console.log('preload');
-    this.load.image(
-        'cloud1',
-        'assets/scenery/overworld/cloud1.png'
-    )
-    this.load.spritesheet(
-        'mario',
-        'assets/entities/mario.png',
-        { frameWidth: 16}
-    )
-    this.load.image(
-        'floorbricks',
-        'assets/scenery/overworld/floorbricks.png',
-        { frameWidth: 32}
-    )
-    this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
+function preload () {
+  this.load.image(
+    'cloud1',
+    'assets/scenery/overworld/cloud1.png'
+  )
+
+  this.load.image(
+    'floorbricks',
+    'assets/scenery/overworld/floorbricks.png'
+  )
+
+  this.load.spritesheet(
+    'mario', // <--- id
+    'assets/entities/mario.png',
+    { frameWidth: 18, frameHeight: 16 }
+  )
+
+  this.load.spritesheet(
+    'goomba',
+    'assets/entities/overworld/goomba.png',
+    { frameWidth: 16, frameHeight: 16 }
+  )
+
+  this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
+} // 1.
+
+function create () {
+  // image(x, y, id-del-asset)
+  this.add.image(100, 50, 'cloud1')
+    .setOrigin(0, 0)
+    .setScale(0.15)
+
+  this.floor = this.physics.add.staticGroup()
+
+  this.floor
+    .create(0, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody()
+
+  this.floor
+    .create(150, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody()
+
+  this.mario = this.physics.add.sprite(50, 100, 'mario')
+    .setOrigin(0, 1)
+    .setCollideWorldBounds(true)
+    .setGravityY(300)
+
+  this.enemy = this.physics.add.sprite(120, config.height - 30, 
+    'goomba')  
+    .setOrigin(0, 1)
+    .setGravityY(300)
+    .setVelocityX(-30)
+
+
+  this.physics.world.setBounds(0, 0, 2000, config.height)
+  this.physics.add.collider(this.mario, this.floor)
+  this.physics.add.collider(this.enemy, this.floor)
+  this.physics.add.collider(this.mario, this.enemy,onHitEnemy)
+
+  this.cameras.main.setBounds(0, 0, 2000, config.height)
+  this.cameras.main.startFollow(this.mario)
+
+  createAnimations(this)
+
+  this.enemy.anims.play('goomba-walk', true)  
+
+  this.keys = this.input.keyboard.createCursorKeys()
 }
 
-function create()   {
-    
-    this.add.image(110,50,'cloud1')
-        .setOrigin(0,0)
-        .setScale(0.15)
+function onHitEnemy (mario, enemy) {
+    if (mario.body.touching.down && enemy.body.touching.up) 
+    {
+      enemy.anims.play('goomba-dead', true)
+      enemy.setVelocityX(0)
+      mario.setVelocityY(-200)
 
-
-
-    // SUELOS    
-    this.floor = this.physics.add.staticGroup()
-
-    this.floor
-        .create(0, config.height - 16, 'floorbricks')
-        .setOrigin(0,.5)
-        .refreshBody()
-     
-
-    this.floor
-        .create(150, config.height - 16, 'floorbricks')
-        .setOrigin(0,.5)
-        .refreshBody()
-
-
-    // MARIO
-    this.mario = this.physics.add.sprite(50,100,'mario')
-        .setOrigin(0,1)
-        .setCollideWorldBounds(true)
-        .setGravityY(300)
-
-    this.physics.world.setBounds(0,0,2000, config.height)
-    this.physics.add.collider(this.mario, this.floor)
-
-    this.cameras.main.setBounds(0, 0, 2000, config.height)
-    this.cameras.main.startFollow(this.mario)   
-
-    this.keys = this.input.keyboard.createCursorKeys()
-    
-
-    // Animaciones
-    createAnimations(this)
-}
-
-function update()   {
-    if (this.mario.isDead) {
-        return
-    }
-    if (this.keys.left.isDown) {
-        this.mario.x -= 2
-        this.mario.anims.play('mario-walk', true)
-        this.mario.flipX = true
-    } else if (this.keys.right.isDown) {
-        this.mario.x += 2
-        this.mario.anims.play('mario-walk', true)
-        this.mario.flipX = false
-    } else if (this.keys.up.isDown) {
-        this.mario.y -= 5
-        this.mario.anims.play('mario-jump', true)
+      setTimeout(() => {
+        enemy.destroy()
+      }, 500)
     } else {
-        this.mario.anims.play('mario-idle', true)
+        
     }
+}
 
-    if (this.keys.up.isDown && this.mario.body.touching.down) {
-        this.mario.setVelocityY(-300)
-        this.mario.anims.play('mario-jump', true)
-    }
+function update () { // 3. continuamente
+  checkControls(this)
 
-    if (this.mario.y >= config.height) {
-        this.mario.isDead = true
-        this.mario.anims.play('mario-dead')
-        this.mario.setCollideWorldBounds(false)
-        this.sound.add('gameover', { volume: 0.4}).play()
+  const { mario, sound, scene } = this
 
-        setTimeout(() => {
-            this.mario.setVelocityY(-350)
-        }, 100)
+  if (mario.y >= config.height) {
+    mario.isDead = true
+    mario.anims.play('mario-dead')
+    mario.setCollideWorldBounds(false)
+    sound.add('gameover', { volume: 0.2 }).play()
 
-        setTimeout(() => {
-            this.scene.restart()
-        }, 2000)
-    }
+    setTimeout(() => {
+      mario.setVelocityY(-350)
+    }, 100)
+
+    setTimeout(() => {
+      scene.restart()
+    }, 2000)
+  }
 }
